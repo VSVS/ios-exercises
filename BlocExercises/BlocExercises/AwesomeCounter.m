3afba0e4a90875b42d137954f9821e6cd564c2be
Robert Vargas <vargasvisuals@gmail.com>
2014-10-09 19:27:33 -0400

    Completed loops

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,27 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+    NSMutableString *result = [[NSMutableString alloc]init];
+    NSString *temp;
+    
+    if (number < otherNumber) {
+        for (NSInteger i=number; i<=otherNumber; i++) {
+            temp = [NSString stringWithFormat:@"%ld", i];
+            [result appendString:temp];
+        }
+    } else if (otherNumber < number) {
+        for (NSInteger i=otherNumber; i<=number; i++) {
+            temp = [NSString stringWithFormat:@"%ld", i];
+            [result appendString:temp];
+        }
+    } else if (number == otherNumber) {
+        temp = [NSString stringWithFormat:@"%ld", number];
+        [result appendString:temp];
+    } else {
+        [result appendString:@"unknown test unit"];
+    }
+ 
+    return result;
 }
 
 @end
